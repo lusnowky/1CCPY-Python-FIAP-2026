c6d9b27fbe23bdfbae15a8d0698afcc0e3a9b53f
lusnowky <lusnowky@gmail.com>
2026-04-29 17:08:35 -0300

    Update

--- Aula03_Modulos_Condicionais/Exercícios/ex10.py
+++ Aula03_Modulos_Condicionais/Exercícios/ex10.py
@@ -12,14 +12,17 @@ if b < c:
 
 if a >= b + c:
     print("NAO FORMA TRIANGULO")
-if a**2 == b**2 + c**2:
-    print("TRIANGULO RETANGULO")
-if a**2 > b**2 + c**2:
-    print("TRIANGULO OBTUSANGULO")
-if a**2 < b**2 + c**2:
-    print("TRIANGULO ACUTANGULO")
+    exit()
+
+else:
+    if a**2 == b**2 + c**2:
+        print("TRIANGULO RETANGULO")
+    elif a**2 > b**2 + c**2:
+        print("TRIANGULO OBTUSANGULO")
+    elif a**2 < b**2 + c**2:
+        print("TRIANGULO ACUTANGULO")
 
 if a == b == c:
     print("TRIANGULO EQUILATERO")
-if (a == b != c) or (a == c != b) or (b == c != a):
+elif (a == b != c) or (a == c != b) or (b == c != a):
     print("TRIANGULO ISOCELES")
