c504558393c3e7d861ccabb2f4751f9cf9fab7bc
lusnowky <lusnowky@gmail.com>
2026-08-17 14:48:56 -0300

    Começo do ex01 da Aula 06

---
+++ Aula06_Matrizes_eExercicios/exercicios/ex01.py
@@ -0,0 +1,6 @@
+# EXERCÍCIO 1 - WHILE
+# Faça um programa que exiba a mensagem “Olá, Mundo”.
+# Essa mensagem deverá ser exibida repetidamente.
+# Ao final de toda iteração da repetição, você deve perguntar ao usuário se ele deseja exibir a mensagem
+# novamente.
+# Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”
