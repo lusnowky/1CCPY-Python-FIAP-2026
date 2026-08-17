3e0e80d89c98f48c64498fdccee79dabfb6bae35
lusnowky <lusnowky@gmail.com>
2026-08-17 16:38:23 -0300

    Terminar de fazer o exercício 3

--- Aula06_Matrizes_Exercicios/exercicios/ex01.py
+++ Aula06_Matrizes_Exercicios/exercicios/ex01.py
@@ -4,3 +4,17 @@
 # Ao final de toda iteração da repetição, você deve perguntar ao usuário se ele deseja exibir a mensagem
 # novamente.
 # Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”
+
+while True:
+    print("Olá, Mundo")
+    print()
+
+    continuar = int(input("Deseja exibir a mensagem novamente? SIM [1] | Não [2]: "))
+    if continuar == 1:
+        continue
+    elif continuar == 2:
+        print("Mensagem cancelada com sucesso!")
+        break
+    else:
+        print("Erro!")
+        break
